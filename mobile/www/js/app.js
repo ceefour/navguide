e48542304dee268c2ab5d81ac6036ccf0634a1ae
Hendy Irawan <ceefour666@gmail.com>
2015-03-24 16:32:54 +0700

    Ionic upgrade and change to sidemenu.

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -3,7 +3,6 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
@@ -11,10 +10,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
@@ -22,87 +21,81 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 })
 
 .config(function($stateProvider, $urlRouterProvider, $compileProvider) {
-
+    
     $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel|app|chrome-extension):/);
     $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel|app|chrome-extension):/);
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+    
   $stateProvider
 
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
-    })
-
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+  
     // Each tab has its own nav history stack:
 
-    .state('tab.dash', {
+    .state('app.dash', {
       url: '/dash',
       views: {
         'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
+          templateUrl: 'templates/app-dash.html',
           controller: 'DashCtrl'
         }
       }
     })
 
-    .state('tab.restareas', {
+    .state('app.restareas', {
       url: '/restareas',
       views: {
         'tab-restareas': {
-          templateUrl: 'templates/tab-restareas.html',
+          templateUrl: 'templates/app-restareas.html',
           controller: 'RestAreasCtrl'
         }
       }
     })
 
-    .state('tab.bpjs', {
+    .state('app.bpjs', {
       url: '/bpjs',
       views: {
         'tab-bpjs': {
-          templateUrl: 'templates/tab-bpjs.html',
+          templateUrl: 'templates/app-bpjs.html',
           controller: 'BpjsCtrl'
         }
       }
     })
   
-    .state('tab.realtime', {
+    .state('app.realtime', {
       url: '/realtime',
       views: {
         'tab-realtime': {
-          templateUrl: 'templates/tab-realtime.html',
+          templateUrl: 'templates/app-realtime.html',
           controller: 'RealtimeCtrl'
         }
       }
     })
   
-    .state('tab.tracker', {
+    .state('app.tracker', {
       url: '/tracker',
       views: {
         'tab-tracker': {
-          templateUrl: 'templates/tab-tracker.html',
+          templateUrl: 'templates/app-tracker.html',
           controller: 'TrackerCtrl'
         }
       }
     })
   
-    .state('tab.account', {
+    .state('app.account', {
       url: '/account',
       views: {
         'tab-account': {
-          templateUrl: 'templates/tab-account.html',
+          templateUrl: 'templates/app-account.html',
           controller: 'AccountCtrl'
         }
       }
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
+  $urlRouterProvider.otherwise('/dash');
 });
-
